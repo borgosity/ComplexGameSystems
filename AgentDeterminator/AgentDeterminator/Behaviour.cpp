bc0a64c77e25f868b0cb9ff986bbd2e397ed2d4a
Phill Banks <phillabanks@gmail.com>
2017-04-25 09:54:35 +1000

    behaviour updates

--- AgentDeterminator/AgentDeterminator/Behaviour.cpp
+++ AgentDeterminator/AgentDeterminator/Behaviour.cpp
@@ -6,7 +6,44 @@ Behaviour::Behaviour()
 {
 }
 
+Behaviour::Behaviour(FuzzyMemberType a_fuzzyMemberType)
+{
+	switch (a_fuzzyMemberType)
+	{
+	case FMT_SHOULDER_LEFT:
+		break;
+	case FMT_SHOULDER_RIGHT:
+		break;
+	case FMT_TRAPEZOID:
+		break;
+	case FMT_TRIANGULAR:
+		break;
+	case FMT_GAUSIAN:
+		break;
+	case FMT_SINGLETON:
+		break;
+	default:
+		break;
+	}
+}
+
+
+Behaviour::Behaviour(float a_startValue, float a_endValaue)
+{
+}
+
+Behaviour::Behaviour(float a_minStartValue, float a_maxPeakValue, float a_minEndVlaue)
+{
+}
+
+Behaviour::Behaviour(float a_minStartValue, float a_maxStartValue, float a_maxEndValue, float a_minEndVlaue)
+{
+}
 
 Behaviour::~Behaviour()
+{
+}
+
+void Behaviour::Update()
 {
 }
--- AgentDeterminator/AgentDeterminator/Behaviour.h
+++ AgentDeterminator/AgentDeterminator/Behaviour.h
@@ -1,16 +1,20 @@
 #pragma once
 #include <string>
 
-enum FuzzyType {
-	FT_SHOULDER_LEFT,
-	FT_SHOULDER_RIGHT,
-	FT_TRAPEZOID,
-	FT_TRIANGULAR
+enum FuzzyMemberType {
+	FMT_SHOULDER_LEFT,
+	FMT_SHOULDER_RIGHT,
+	FMT_TRAPEZOID,
+	FMT_TRIANGULAR,
+	FMT_GAUSIAN,
+	FMT_SINGLETON
 };
 
 class Behaviour
 {
 public:
+	Behaviour();
+	Behaviour(FuzzyMemberType	a_fuzzyMemberType);
 	Behaviour(float a_startValue, float a_endValaue);
 	Behaviour(float a_minStartValue, float a_maxPeakValue, float a_minEndVlaue);
 	Behaviour(float a_minStartValue, float a_maxStartValue, float a_maxEndValue, float a_minEndVlaue);
@@ -21,7 +25,7 @@ public:
 
 protected:
 	std::string m_sName;
-	FuzzyType m_fuzzyType;
+	FuzzyMemberType m_fuzzyMemberType;
 
 	float m_fLeftValue;
 	float m_fRightValue;
